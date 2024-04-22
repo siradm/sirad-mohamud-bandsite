db592a35ed2b0d4e8da908dfb9d92cb279c5932c
Sirad Mohamud <siradmohamud9@gmail.com>
2024-04-21 20:38:02 -0700

    finished shows page styling and js for shows

--- scripts/build-shows-pages.js
+++ scripts/build-shows-pages.js
@@ -1,1 +1,123 @@
-test()
+// Shows Array
+
+const showsArray = [
+    {
+      Date: "Mon Sept 09 2024",
+      Venue: "Ronald Lane",
+      Location: "San Francisco, CA",
+    },
+    {
+      Date: "Tue Sept 17 2024",
+      Venue: "Pier 3 East",
+      Location: "San Francisco, CA",
+    },
+    {
+      Date: "Sat Oct 21 2024",
+      Venue: "View Lounge",
+      Location: "San Francisco, CA",
+    },
+    {
+      Date: "Sat Nov 16 2024",
+      Venue: "Hyatt Agency",
+      Location: "San Francisco, CA",
+    },
+    {
+      Date: "Fri Nov 29 2024",
+      Venue: "Moscow Center",
+      Location: "San Francisco, CA",
+    },
+    {
+      Date: "Wed Dec 18 2024",
+      Venue: "Press Club",
+      Location: "San Francisco, CA",
+    },
+  ];
+  
+  // Shows Section
+  const showSection = document.querySelector(".shows-section");
+  
+  // Shows Section Header
+  
+  const displayShowsSectionHeader = () => {
+    const sectionHeader = document.createElement("h2");
+    sectionHeader.classList.add("shows-section__header");
+    sectionHeader.innerText = "Shows";
+    showSection.appendChild(sectionHeader);
+  };
+  
+  displayShowsSectionHeader();
+  
+  // Shows table Construction
+  
+  const constructShowTable = () => {
+    const showTable = document.createElement("table");
+    showTable.classList.add("show-table");
+  
+    const showTableHead = document.createElement("thead");
+    showTableHead.classList.add("show-table__header-contain");
+  
+    const showTableHeadRow = document.createElement("tr");
+    showTableHeadRow.classList.add("show-table__header");
+  
+    const showHeaderTextDate = document.createElement("th");
+    showHeaderTextDate.classList.add("show-table__header--text");
+    showHeaderTextDate.innerText = "DATE";
+  
+    const showHeaderTextVenue = document.createElement("th");
+    showHeaderTextVenue.classList.add("show-table__header--text");
+    showHeaderTextVenue.innerText = "VENUE";
+  
+    const showHeaderTextLocation = document.createElement("th");
+    showHeaderTextLocation.classList.add("show-table__header--text");
+    showHeaderTextLocation.innerText = "LOCATION";
+  
+    const showTableBody = document.createElement("tbody");
+    showTableBody.classList.add("show-table__body");
+  
+    showSection.appendChild(showTable);
+    showTable.appendChild(showTableHead);
+    showTableHead.appendChild(showTableHeadRow);
+    showTableHeadRow.appendChild(showHeaderTextDate);
+    showTableHeadRow.appendChild(showHeaderTextVenue);
+    showTableHeadRow.appendChild(showHeaderTextLocation);
+    showTable.appendChild(showTableBody);
+  
+    for (let i = 0; i < showsArray.length; i++) {
+      const tableRow = document.createElement("tr");
+      tableRow.classList.add("show-table__show-row");
+  
+      const showDate = document.createElement("td");
+      showDate.classList.add("show-table__text--date");
+      showDate.innerText = showsArray[i].Date;
+  
+      const showVenue = document.createElement("td");
+      showVenue.classList.add("show-table__text");
+      showVenue.innerText = showsArray[i].Venue;
+  
+      const showLocation = document.createElement("td");
+      showLocation.classList.add("show-table__text");
+      showLocation.innerText = showsArray[i].Location;
+  
+      const ticketButton = document.createElement("button");
+      ticketButton.classList.add("show-table__button");
+      ticketButton.innerText = "BUY TICKETS";
+  
+      showTableBody.appendChild(tableRow);
+      tableRow.appendChild(showDate);
+      tableRow.appendChild(showVenue);
+      tableRow.appendChild(showLocation);
+      tableRow.appendChild(ticketButton);
+    }
+  };
+  
+  constructShowTable(showsArray);
+  
+  // Active row event listener
+  
+  const activeRow = document.querySelectorAll(".show-table__show-row");
+  
+  activeRow.forEach((activeRow) => {
+    activeRow.addEventListener("click", (event) => {
+      activeRow.classList.add("show-table__show-row:active");
+    });
+  });
